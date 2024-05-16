380a9513f205713f26cc086e0816468309b76cd0
Vineet Karwa <vineetkarwa123@gmail.com>
2024-05-16 19:05:45 +0530

    Feat: Changing neccesary details

--- backend/constants/config.js
+++ backend/constants/config.js
@@ -8,6 +8,6 @@ const corsOptions = {
   credentials: true,
 };
 
-const CHATTU_TOKEN = "chattu-token";
+const TALKSPOT_TOKEN = "talkspot-token";
 
-export { corsOptions, CHATTU_TOKEN };
+export { corsOptions, TALKSPOT_TOKEN };
